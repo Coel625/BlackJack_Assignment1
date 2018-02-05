434a6c75bd0ea5ece680fd42e5c01652b7295e62
Coel625 <btaylor40@my.apsu.edu>
2018-02-04 20:48:58 -0600

    Made some buttons and set up the general design of the highscore activity; will later turn the last two columns into a single column that uses an array of values

--- app/src/main/java/edu/apsu/csci/blackjack_assignment1/GameActivity.java
+++ app/src/main/java/edu/apsu/csci/blackjack_assignment1/GameActivity.java
@@ -1,7 +1,10 @@
 package edu.apsu.csci.blackjack_assignment1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GameActivity extends Activity {
 
@@ -9,6 +12,17 @@ public class GameActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        Button b = (Button) findViewById(R.id.score_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        HighscoreActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
--- app/src/main/java/edu/apsu/csci/blackjack_assignment1/HighscoreActivity.java
+++ app/src/main/java/edu/apsu/csci/blackjack_assignment1/HighscoreActivity.java
@@ -1,7 +1,10 @@
 package edu.apsu.csci.blackjack_assignment1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HighscoreActivity extends Activity {
 
@@ -9,5 +12,16 @@ public class HighscoreActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_highscore);
+
+        Button b = (Button) findViewById(R.id.return_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        StartActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
