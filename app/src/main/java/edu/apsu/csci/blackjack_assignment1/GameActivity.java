e04a6210e042e6979bacc16479525c856d918161
WalkerC <christophermwalker@gmail.com>
2018-02-10 11:00:12 -0500

    wired buttons

--- app/src/main/java/edu/apsu/csci/blackjack_assignment1/GameActivity.java
+++ app/src/main/java/edu/apsu/csci/blackjack_assignment1/GameActivity.java
@@ -6,23 +6,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class GameActivity extends Activity {
+public class GameActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        Button b = (Button) findViewById(R.id.score_button);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        int ids[]={R.id.hit_btn, R.id.hold_btn, R.id.schuffle_btn, R.id.deal_btn, R.id.score_button};
+        for (int id : ids) {
+            Button btn=(Button) findViewById(id);
+            btn.setOnClickListener((View.OnClickListener) this);
+        }
+    }
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.score_button) {
                 Intent intent = new Intent(
                         getApplicationContext(),
                         HighscoreActivity.class);
                 startActivity(intent);
             }
-        });
+        }
     }
-}
 
